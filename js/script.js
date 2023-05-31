2bc172895e81cf39f2384206381102a83d45c928
b1art <70599221+b1art@users.noreply.github.com>
2023-05-31 14:38:03 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.bar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -15,6 +23,12 @@ window.onscroll = () => {
             });
         };
     });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.toggle.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
 ScrollReveal({
